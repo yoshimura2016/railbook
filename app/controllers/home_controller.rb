2be83ecfb0ad089ede443051eb9f8455a66641dc
Yoshimura <Yoshimura@example.com>
2018-10-17 11:44:04 +0900

    第7章まで

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def top
+    @name = '出商太郎'
+
+    @today = Date.today
+    week = ['日','月','火','水','木','金','土']
+    @yobi = week[@today.wday]
   end
 end
